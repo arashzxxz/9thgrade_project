263c3948f02c579140884913b69f9d622e0f0a83
Arash shakarami <arash8989arash@gmail.com>
2024-10-29 23:20:43 +0330

    added_app_icon

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class mainw(QMainWindow):
         #create main window
         super(mainw,self).__init__()
         self.setWindowTitle('FitLife')
+        self.setWindowIcon(QIcon("C:/Users/r/Contacts/Desktop/9thgrade_project/assets/logo.png"))
         self.Width = 1000
         self.height = int(0.618 * self.Width)
         self.resize(self.Width, self.height)
